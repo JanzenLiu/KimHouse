be663a795bbd85adc99de16e806ca12c79c2409f
= <chengtsung_liu@yahoo.com>
2017-06-15 21:52:13 +0800

    analysis features with only 3 unique values

--- Preprocess/raw.py
+++ Preprocess/raw.py
@@ -1,5 +1,7 @@
 from reader import *
 
+cr = Reader(combine)
+
 log(cr.cat_feats,
 	title=format("%d Categorical Features:" % len(cr.cat_feats)))
 log("...",
@@ -26,12 +28,23 @@ log(cr.df[cr.num_feats].nunique().sort_values().values,
 	"Num of unique values of numeric features")
 
 
-num_feats2 = cr.df[cr.num_feats].loc[:,(cr.df[cr.num_feats].nunique() == 2)].columns
-log(num_feats2,
+num_feats_2 = cr.df[cr.num_feats].loc[:,(cr.df[cr.num_feats].nunique() == 2)].columns
+log(num_feats_2,
 	"Numeric Features with only 2 values")
-for feat in num_feats2:
+for feat in num_feats_2:
 	log(cr.df[feat].value_counts())
 log(cr.df['mosque_count_3000'].value_counts())
 log(cr.df['mosque_count_5000'].value_counts())
 # From above we can see that how rare mosque is in the living area above in Russia
 # Therefore we can consider drop those feature concerning mosque
+
+num_feats_3 = cr.df[cr.num_feats].loc[:,(cr.df[cr.num_feats].nunique() == 3)].columns
+log(num_feats_3,
+	"Numeric Features with only 3 values")
+# Among which those two mosque things we have already studied
+for feat in num_feats_3.drop(['mosque_count_3000', 'mosque_count_5000']):
+	log(cr.df[feat].value_counts())
+# It's about school, so what about without the criteion top20?
+log(cr.df['school_education_centers_raion'].value_counts())
+# The number of values is a bit more, but still, only 14, with maximum 14 and minimum 0
+
