dc7bf2617b5958b52d0c12edf4f037c4e4e5a176
= <chengtsung_liu@yahoo.com>
2017-06-15 22:44:41 +0800

    add selecting features by certain nunique counts (range)

--- Preprocess/reader.py
+++ Preprocess/reader.py
@@ -1,5 +1,6 @@
 from header import *
 
+# TODO(JanzenLiu): add function of selecting columns with certain nunique() number
 class Reader:
 	def __init__(self, df):
 		self.df = df
@@ -12,4 +13,12 @@ class Reader:
 			else:
 				self.num_feats.append(col)
 
-cr = Reader(combine)
+	def feats_with_nunique(self, lower, upper=None, numfeats=True):
+		if(numfeats):
+			cols = self.num_feats
+		else:
+			cols = self.df.columns
+		if(not upper):
+			return self.df[cols].loc[:,(self.df[cols].nunique() == lower)].columns
+		else:
+			return self.df[cols].loc[:,((self.df[cols].nunique() >= lower) * (self.df[cols].nunique() < upper))].columns
