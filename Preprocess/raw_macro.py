b749d1bbf7fda3537488836eda2dd4c1f88a2cf7
= <chengtsung_liu@yahoo.com>
2017-06-16 01:23:59 +0800

    add simple analysis on features with up to 7 values in macro

--- Preprocess/raw_macro.py
+++ Preprocess/raw_macro.py
@@ -1,6 +1,8 @@
 from header import *
 from reader import Reader
 
+# TODP(Janzen): to plot the fluctuation of all macro index by the time series
+
 mr = Reader(macro, "log_macro.txt")
 mr.log(mr.cat_feats,
 	title=format("%d Categorical Features:" % len(mr.cat_feats)))
@@ -31,3 +33,15 @@ mr.log(
 	mr.df[mr.df["child_on_acc_pre_school"] == "7,311"]["timestamp"].apply(lambda x: x[:4]).value_counts(),
 	"Year Distribution of records with '7,311' child_on_acc_pre_school")
 # Well, it's very likely that those feature appear with year. We will study them later
+
+
+mr.log(mr.df[mr.num_feats].nunique().sort_values().values, "Num of unique values of numeric features")
+# Seems good. There're quite a lot of values with only few unique values. And possibly appear with year?
+
+feats_7m = mr.feats_with_nunique(1, 8, "num") # stands for 7 minus
+mr.log(feats_7m, "Numeric features with up to 7 values")
+mr.log(len(feats_7m), "And number of feats") # 64
+mr.log("", "The distribution of them")
+for feat in feats_7m:
+	mr.log(mr.df[feat].value_counts())
+# Though not examine yet, we can say they're highly possibly to appear with year
