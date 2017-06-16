32a54cd75d9dc4ce28c550ac5fd33f94e6029d5a
= <chengtsung_liu@yahoo.com>
2017-06-16 22:32:55 +0800

    seperate the processing of feature name out to be an individual function and add some simple analysis on cafe features by seperating those counts and prices features

--- Analysis/cafe.py
+++ Analysis/cafe.py
@@ -2,6 +2,23 @@ import sys
 sys.path.append('../')
 from Common import *
 
+def process_feat_name(df):
+	labels = []
+	for feat in list(df.columns):
+		feat = feat.replace("count", "#")
+		feat = feat.split("_")
+		feat.remove(dist)
+		feat.remove("cafe")
+		if("sum" in feat):
+			feat.remove("sum")
+		if("min" in feat or "max" in feat):
+			feat.remove("avg")
+		if("price" in feat):
+			feat.remove("price")
+		feat = " ".join(feat)
+		labels.append(feat)
+	return labels
+
 hr = HouseReader()
 cafs = hr.df[hr.caf_feats].drop(['catering_km'], 1)
 
@@ -26,20 +43,7 @@ pos = 230
 plt.figure(figsize=(15,12))
 for item in caf_by_dist:
 	dist = item.columns[-1].split("_")[2]
-	labels = []
-	for feat in list(item.columns):
-		feat = feat.replace("count", "#")
-		feat = feat.split("_")
-		feat.remove(dist)
-		feat.remove("cafe")
-		if("sum" in feat):
-			feat.remove("sum")
-		if("min" in feat or "max" in feat):
-			feat.remove("avg")
-		if("price" in feat):
-			feat.remove("price")
-		feat = " ".join(feat)
-		labels.append(feat)
+	labels = process_feat_name(item)
 	pos += 1
 	plt.subplot(pos)
 	sns.heatmap(item.corr(), square=True, xticklabels=labels, yticklabels=labels, vmin=0.15)
@@ -55,3 +59,37 @@ plt.show()
 # 3. The correlation between the price statistics and cafe counts decreases as the distance increases.
 # 4. When the distance is 500, the price statistics are correlated to the counts of expensive cafe noticably, though not to a high level
 # Maybe we can consider those 500-distance data more important? We will learn more later
+
+pos = 230
+plt.figure(figsize=(15,12))
+for item in caf_by_dist:
+	df = pd.concat([item.iloc[:,4:], item.iloc[:,:1]], axis=1)
+	dist = item.columns[-1].split("_")[2]
+	labels = process_feat_name(df)
+	pos += 1
+	plt.subplot(pos)
+	sns.heatmap(df.corr(), square=True, xticklabels = labels, yticklabels=labels, vmin=0.6)
+	plt.xticks(rotation=30, fontsize=8)
+	plt.yticks(rotation=30, fontsize=8)
+	plt.title("Cafe counts by distance: %s" % dist)
+plt.subplots_adjust(left=0.1, right=0.9, wspace=0.2, hspace=0.4)
+plt.savefig("../Figure/Raw/Cafe/count_corr_each_dist.png")
+plt.show()
+
+pos = 230
+plt.figure(figsize=(15,12))
+for item in caf_by_dist:
+	df = item.iloc[:,1:4]
+	dist = item.columns[0].split("_")[-1]
+	labels = process_feat_name(df)
+	pos += 1
+	plt.subplot(pos)
+	sns.heatmap(df.corr(), square=True, xticklabels = labels, yticklabels=labels, vmin=0.98)
+	plt.xticks(rotation=30, fontsize=8)
+	plt.yticks(rotation=30, fontsize=8)
+	plt.title("Cafe avg bill by distance: %s" % dist)
+plt.subplots_adjust(left=0.1, right=0.9, wspace=0.2, hspace=0.4)
+plt.savefig("../Figure/Raw/Cafe/price_corr_each_dist.png")
+plt.show()
+# from the pictures we can see that, with the distance fixed, counts are very highly correlated, so do those price statistics
+# Maybe we can reconstruct all those cafe features to no more than 5 features, which will significantly save time and computational power for us
