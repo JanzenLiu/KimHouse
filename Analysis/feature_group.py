dc5b9a342b6ba0180c56acf2140e6f8db5f20ca3
= <chengtsung_liu@yahoo.com>
2017-06-17 15:39:18 +0800

    add customized correlation plotter function for FeatureGroup class

--- Analysis/feature_group.py
+++ Analysis/feature_group.py
@@ -3,9 +3,12 @@ sys.path.append('../')
 from Common import *
 
 class FeatureGroup:
-	def __init__(self, df, figdir=None, logpath=None):
+	def __init__(self, df, name, figdir=None, logpath=None):
+		assert df and name
 		self.df = df
+		self.name = name
 		self.corr = df.corr()
+		self.set_featname_processor()
 		if(figdir):
 			self.set_figdir(figdir)
 		if(logfile):
@@ -23,10 +26,12 @@ class FeatureGroup:
 		logname = os.path.basename(logpath)
 		if(not os.path.exists(logdir)):
 			os.makedirs(logdir)
+		if(not logname):
+			logname = format("%s.txt" % self.name)
 		self.logdir = logdir
 		self.logname = logname
 
-	def set_featname_processor(self, dicts, delimiter, jointer):
+	def set_featname_processor(self, dicts={}, delimiter="@@@", jointer="@@@"):
 		assert type(dicts) == dict
 		assert type(delimiter) == str
 		assert type(jointer) == str
@@ -40,3 +45,20 @@ class FeatureGroup:
 				cols.append(col)
 			return cols
 		self.process_featname = process_featname
+
+	def set_corr_plotter(self, labels, title=None):
+		def plot_corr(self, save=False):
+			ax = sns.heatmap(self.corr, square=True)
+		    if(labels):
+				ax.set_xticklabels(labels)
+				ax.set_yticklabels(labels)
+				plt.xticks(rotation=30)
+				plt.yticks(rotation=30)
+			if(not title):
+				title = self.name
+			plt.title(title)
+		    if(save):
+		    	figpath = os.path.join(self.figdir, format("%s.png" % self.name))
+		        plt.savefig(figpath)
+		    plt.show()
+		self.plot_corr = plot_corr
